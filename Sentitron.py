9fed405de2d75c637b13f16ed5250163ce308358
Sergei Karulin <sergey.karulin@gmail.com>
2023-11-30 16:03:59 +0400

    Working on Sentitron.py

--- Sentitron.py
+++ Sentitron.py
@@ -27,6 +27,11 @@ class SentytronNetwork:
         self.inner_to_io_mediators = torch.zeros(inner_layer_size, self.input_output_size)
         self._init_synapses(self.inner_to_io_weights, self.inner_to_io_mediators)
 
+        # 5. Initialize synapse weights and mediator amounts for inner layer connections
+        self.inner_to_inner_weights = torch.zeros(inner_layer_size, inner_layer_size)
+        self.inner_to_inner_mediators = torch.zeros(inner_layer_size, inner_layer_size)
+        self._init_synapses(self.inner_to_inner_weights, self.inner_to_inner_mediators)
+
     def _init_synapses(self, weights, mediators):
         # Initialize synapses with random connections and weights
         for neuron_idx in range(weights.shape[0]):
@@ -37,13 +42,43 @@ class SentytronNetwork:
             mediators[neuron_idx, connected_neurons] = torch.randint(0, 2, (len(connected_neurons),))
 
     # Define other necessary functions (exchange, mediator_diffuse, self_excitate, etc.)
+    def self_excitate(self):
+        # Calculate the increase in self_excitate_rate
+        for neuron_idx in range(self.inner_layer_size):
+            excitation_increase = torch.sum(
+                self.io_to_inner_weights[:, neuron_idx] * self.io_to_inner_mediators[:, neuron_idx]
+            ) + torch.sum(
+                self.inner_to_inner_weights[:, neuron_idx] * self.inner_to_inner_mediators[:, neuron_idx]
+            )
+            self.self_excitate_rate[neuron_idx] += excitation_increase
 
-# English and Russian alphabets, digits
+            # Check for excharge
+            if self.self_excitate_rate[neuron_idx] >= self.excite_threshold:
+                self.self_excitate_rate[neuron_idx] -= self.excite_threshold
+                # Increase mediators for outbound connections
+                self.inner_to_inner_mediators[neuron_idx, :] += 1
+                self.inner_to_io_mediators[neuron_idx, :] += 1
+
+    def diffuse(self):
+        # Deduct all mediator concentrations by the decay rate
+        self.io_to_inner_mediators -= self.decay_rate
+        self.io_to_inner_mediators.clamp_(0, 1)
+        self.inner_to_inner_mediators -= self.decay_rate
+        self.inner_to_inner_mediators.clamp_(0, 1)
+        self.inner_to_io_mediators -= self.decay_rate
+        self.inner_to_io_mediators.clamp_(0, 1)
+
+    def tick(self):
+        # Call self_excitate and diffuse functions
+        self.self_excitate()
+        self.diffuse()
+
+# Define the symbols array
 english_alphabet = string.ascii_letters  # Contains both lowercase and uppercase English letters
 russian_alphabet = ''.join([chr(i) for i in range(1040, 1104)]) + 'Ёё'  # Contains both lowercase and uppercase Russian letters, including Ё and ё
 digits = string.digits
-# 20 most used special characters
 special_characters = '. , % $ + - * / = & @ # ! ? : ; \' " ( )'
+
 # Combining all symbols
 symbols = list(english_alphabet + russian_alphabet + digits + special_characters)
 
@@ -52,3 +87,5 @@ inner_layer_size = 1000
 max_synapses = 1000
 
 network = SentytronNetwork(symbols, inner_layer_size, max_synapses)
+for _ in range(1000):  # Run for 1000 ticks
+    network.tick()
