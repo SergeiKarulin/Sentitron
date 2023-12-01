9a9a791565c1f732e77811b63b5b19d9283a29f1
Sergei Karulin <sergey.karulin@gmail.com>
2023-12-01 18:42:33 +0400

    Ascilating )

--- Sentitron.py
+++ Sentitron.py
@@ -1,91 +1,82 @@
 import torch
-import numpy as np
-import string
+import random
 
-class SentytronNetwork:
-    def __init__(self, symbols, inner_layer_size, max_synapses):
-        # 1. Initialize parameters
-        self.symbols = symbols
-        self.input_output_size = len(symbols)
-        self.inner_layer_size = inner_layer_size
-        self.decay_rate = torch.rand(1).item()
-        self.excite_threshold = 1
-        self.self_excitate_rate = torch.rand(inner_layer_size)
-        self.max_synapses = max_synapses
+class NeuralNet:
+    def __init__(self, size, max_connections=3, min_connections=1, loop_probability=0.1):
+        self.size = size
+        self.action_potential = 2.0
+        self.decay_rate = 0.5
+        self.fire_strength = 2
+        self.neuron_layer = torch.zeros(size, size)
+        self.synapse_weights = torch.zeros(size, size, size, size)
+        self.synapse_mediators = torch.zeros(size, size, size, size)
+        self.init_synapses(max_connections, min_connections, loop_probability)
+        
+        # Initialize a random neuron with depolarization value of 10
+        random_neuron_x = random.randint(0, size - 1)
+        random_neuron_y = random.randint(0, size - 1)
+        self.neuron_layer[random_neuron_x, random_neuron_y] = 111
 
-        # 2. Initialize neuron layers
-        self.input_output_excitations = torch.zeros(self.input_output_size)
-        self.inner_excitations = torch.zeros(inner_layer_size)
+    def init_synapses(self, max_connections, min_connections, loop_probability, std_dev=1.0):
+        # Create a meshgrid for neuron coordinates
+        x_coords, y_coords = torch.meshgrid(torch.arange(self.size), torch.arange(self.size))
 
-        # 3. Initialize synapse weights and mediator amounts for input/output to inner layer
-        self.io_to_inner_weights = torch.zeros(self.input_output_size, inner_layer_size)
-        self.io_to_inner_mediators = torch.zeros(self.input_output_size, inner_layer_size)
-        self._init_synapses(self.io_to_inner_weights, self.io_to_inner_mediators)
+        # Calculate distances between each pair of neurons
+        x_diff = x_coords.unsqueeze(2).unsqueeze(3) - x_coords.unsqueeze(0).unsqueeze(1)
+        y_diff = y_coords.unsqueeze(2).unsqueeze(3) - y_coords.unsqueeze(0).unsqueeze(1)
+        distances = torch.sqrt(x_diff**2 + y_diff**2)
 
-        # 4. Initialize synapse weights and mediator amounts for inner to input/output layer
-        self.inner_to_io_weights = torch.zeros(inner_layer_size, self.input_output_size)
-        self.inner_to_io_mediators = torch.zeros(inner_layer_size, self.input_output_size)
-        self._init_synapses(self.inner_to_io_weights, self.inner_to_io_mediators)
+        # Apply Gaussian function to distances
+        gaussian_prob = torch.exp(-distances**2 / (2 * std_dev**2))
 
-        # 5. Initialize synapse weights and mediator amounts for inner layer connections
-        self.inner_to_inner_weights = torch.zeros(inner_layer_size, inner_layer_size)
-        self.inner_to_inner_mediators = torch.zeros(inner_layer_size, inner_layer_size)
-        self._init_synapses(self.inner_to_inner_weights, self.inner_to_inner_mediators)
+        # Make sure self-connection probability adheres to the loop_probability
+        loop_indices = torch.arange(self.size)
+        gaussian_prob[loop_indices, loop_indices, loop_indices, loop_indices] *= loop_probability
 
-    def _init_synapses(self, weights, mediators):
-        # Initialize synapses with random connections and weights
-        for neuron_idx in range(weights.shape[0]):
-            connected_neurons = np.random.choice(range(weights.shape[1]), 
-                                                 size=np.random.randint(0, self.max_synapses), 
-                                                 replace=False)
-            weights[neuron_idx, connected_neurons] = torch.rand(len(connected_neurons))
-            mediators[neuron_idx, connected_neurons] = torch.randint(0, 2, (len(connected_neurons),))
+        for i in range(self.size):
+            for j in range(self.size):
+                # Determine potential connections based on Gaussian probabilities
+                probs = gaussian_prob[i, j].flatten()
+                connections = random.randint(min_connections, max_connections)
+                synapse_indices = torch.multinomial(probs, connections, replacement=False)
 
-    # Define other necessary functions (exchange, mediator_diffuse, self_excitate, etc.)
-    def self_excitate(self):
-        # Calculate the increase in self_excitate_rate
-        for neuron_idx in range(self.inner_layer_size):
-            excitation_increase = torch.sum(
-                self.io_to_inner_weights[:, neuron_idx] * self.io_to_inner_mediators[:, neuron_idx]
-            ) + torch.sum(
-                self.inner_to_inner_weights[:, neuron_idx] * self.inner_to_inner_mediators[:, neuron_idx]
-            )
-            self.self_excitate_rate[neuron_idx] += excitation_increase
+                for idx in synapse_indices:
+                    x, y = idx // self.size, idx % self.size
+                    self.synapse_weights[i, j, x, y] = torch.rand(1).item()
 
-            # Check for excharge
-            if self.self_excitate_rate[neuron_idx] >= self.excite_threshold:
-                self.self_excitate_rate[neuron_idx] -= self.excite_threshold
-                # Increase mediators for outbound connections
-                self.inner_to_inner_mediators[neuron_idx, :] += 1
-                self.inner_to_io_mediators[neuron_idx, :] += 1
+
+    def recalculate_depolarization(self):
+        input_sum = torch.sum(self.synapse_weights * self.synapse_mediators, dim=(0, 1))
+        self.neuron_layer += input_sum
+        firing_neurons = self.neuron_layer >= self.action_potential
+        if firing_neurons.any():
+            self.fire(firing_neurons)
+
+    def fire(self, firing_neurons):
+        indices = firing_neurons.nonzero(as_tuple=True)
+        for x, y in zip(*indices):
+            self.neuron_layer[x, y] -= self.action_potential
+            out_synapses = self.synapse_weights[x, y, :, :]
+            total_out = torch.sum(out_synapses > 0, dtype=torch.float32)
+            if total_out > 0:
+                self.synapse_mediators[x, y, :, :] += (self.fire_strength / total_out) * (out_synapses > 0)
+
 
     def diffuse(self):
-        # Deduct all mediator concentrations by the decay rate
-        self.io_to_inner_mediators -= self.decay_rate
-        self.io_to_inner_mediators.clamp_(0, 1)
-        self.inner_to_inner_mediators -= self.decay_rate
-        self.inner_to_inner_mediators.clamp_(0, 1)
-        self.inner_to_io_mediators -= self.decay_rate
-        self.inner_to_io_mediators.clamp_(0, 1)
+        self.synapse_mediators -= self.decay_rate
+        self.synapse_mediators.clamp_(0)
 
-    def tick(self):
-        # Call self_excitate and diffuse functions
-        self.self_excitate()
+    def beat(self):
+        self.recalculate_depolarization()
         self.diffuse()
+        return self.neuron_layer
 
-# Define the symbols array
-english_alphabet = string.ascii_letters  # Contains both lowercase and uppercase English letters
-russian_alphabet = ''.join([chr(i) for i in range(1040, 1104)]) + 'Ёё'  # Contains both lowercase and uppercase Russian letters, including Ё and ё
-digits = string.digits
-special_characters = '. , % $ + - * / = & @ # ! ? : ; \' " ( )'
+# Initialize the neural network
+neural_net = NeuralNet(size=10)
 
-# Combining all symbols
-symbols = list(english_alphabet + russian_alphabet + digits + special_characters)
-
-# Example usage
-inner_layer_size = 1000
-max_synapses = 1000
-
-network = SentytronNetwork(symbols, inner_layer_size, max_synapses)
-for _ in range(1000):  # Run for 1000 ticks
-    network.tick()
+# Run the beat function every 0.5 seconds
+import time
+for _ in range(10):  # Run for 10 beats as an example
+    neural_net.beat()
+    print("Updated Neuron Layer:\n", neural_net.neuron_layer)
+    time.sleep(0.5)
